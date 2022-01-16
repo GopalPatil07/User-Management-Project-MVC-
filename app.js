b86d02f85d5a8ae8258e3f4ef0ab4758eb2ddc27
GopalPatil07 <gopalvishnupatil@gmail.com>
2022-01-16 17:41:45 +0530

    created the handlebars

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
-
+const exphbs = require('express-handlebars');
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.engine('hbs' , exphbs({extname:'.hbs'}));
+app.set("view engine" , 'hbs');
 
 app.listen(port, ()=>{
     console.log(`listening to port number ${port}`);
