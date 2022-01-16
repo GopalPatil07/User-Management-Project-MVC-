b4fa59638ec7c6e65e13fbf167c48449f619ebb4
GopalPatil07 <gopalvishnupatil@gmail.com>
2022-01-16 18:24:45 +0530

    created one view page

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
-app.engine('hbs' , exphbs({extname:'.hbs'}));
+app.engine('hbs' , 
+    exphbs({extname: 'hbs'})
+)
 app.set("view engine" , 'hbs');
-
+app.get("/", (req, res)=>{
+    res.render('home')
+})
 app.listen(port, ()=>{
     console.log(`listening to port number ${port}`);
 });
