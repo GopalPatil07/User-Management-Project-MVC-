692e9392c755b2b414960926a1846a862af9d552
GopalPatil07 <gopalvishnupatil@gmail.com>
2022-01-17 18:17:13 +0530

    created one view page

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars')
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -7,9 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
-app.engine('hbs' , 
-    exphbs({extname: 'hbs'})
-)
+app.engine('hbs' , exphbs.engine({extname: 'hbs'}));
 app.set("view engine" , 'hbs');
 app.get("/", (req, res)=>{
     res.render('home')
