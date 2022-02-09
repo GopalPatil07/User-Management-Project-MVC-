caff3b0f9a5ad42827ee6e2b65bfc1f5792ebb20
sunil <sunil@smalldaytech.com>
2022-02-09 13:00:17 +0530

    purchase product

--- app.js
+++ app.js
@@ -16,17 +16,13 @@ app.post("/addProduct", router);
 app.get("/addDealer", router);
 app.get("/sellItem", router);
 app.get("/products", router);
-<<<<<<< HEAD
 app.get("/purchaseProduct", router);
 app.post("/purchaseProduct", router);
-=======
-<<<<<<< HEAD
+
 app.get("/editProduct/:productId", router);
 app.get("updateProduct/:productId", router);
-=======
 app.get("/purchaseProduct", router)
->>>>>>> 059fc8eb4739dca60f4cfb4bbfba8d62cf356180
->>>>>>> 09fb85489499f1d0506327d1f7dc068cd7a2d50c
+
 app.listen(port, ()=>{
     console.log(`listening to port number ${port}`);
 });
